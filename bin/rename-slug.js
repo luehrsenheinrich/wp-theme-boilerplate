b8b0c61e048f5daeeb520e1dda60af1ae503a1e6
Christopher Kanitz <ckanitz@users.noreply.github.com>
2019-08-30 11:48:55 +0200

    Add/slug rename (#54)
    * add autoloader, namespaces, OOP/Component base from WP Rig
    
    * Update functions.php
    
    * move components to subfolder, remove nav walker, change menu component functions
    
    * remove php7 featured typehints, lint
    
    * make nav item names translatable
    
    * add class_names function as component
    
    * lint, remove old classNames.php file
    
    * improve code readability
    
    * add automatic feed links theme support
    
    * remove var dump
    
    * simpler structure (for now), move theme supports to component
    
    * move textdomain loading to component
    
    * move content width to component
    
    * move enqueue scripts & styles to component
    
    * lint
    
    * add shorten_text function
    
    * remove script/style enqueue from functions
    
    * add scripts
    
    * add npm shorthand
    
    * update rename reporting

---
+++ bin/rename-slug.js
@@ -0,0 +1,67 @@
+// Include prompt module.
+const prompt = require( 'prompt' );
+const isEmpty = require( 'lodash/isEmpty' );
+const forEach = require( 'lodash/forEach' );
+const replace = require( 'replace-in-file' );
+
+// This json object is used to configure what data will be retrieved from command line.
+var prompt_attributes = [
+	{
+		name: 'oldSlug',
+		type: 'string',
+		hidden: false,
+		default: '_lhtbp',
+	},
+	{
+		name: 'newSlug',
+		type: 'string',
+		hidden: false,
+	},
+];
+
+// Start the prompt to read user input.
+prompt.start();
+
+// Prompt and get user input then display those data in console.
+prompt.get( prompt_attributes, function ( err, result ) {
+	if ( err ) {
+		console.log( err );
+		return 1;
+	} else {
+		// Get user input from result object.
+		const oldSlug = result.oldSlug;
+		const newSlug = result.newSlug;
+		const fallbackSlug = oldSlug.startsWith( '_' ) ? `bb${ oldSlug.substring( 1 ) }` : false;
+
+		let from = [ new RegExp( oldSlug, 'g' ) ];
+
+		if ( fallbackSlug ) {
+			from.push( new RegExp( fallbackSlug, 'g' ) );
+		}
+
+		let replaceMap = {};
+		replaceMap[ oldSlug ] = newSlug;
+
+		if ( fallbackSlug ) {
+			replaceMap[ fallbackSlug ] = newSlug;
+		}
+
+		const dir = __dirname;
+		const dirBase = dir.substring( 0, dir.length - 4 );
+
+		const options = {
+			files: dirBase + '/**/*.*',
+			from: from,
+			to: newSlug,
+			ignore: [ '**/node_modules/**' ]
+		};
+
+		const results = replace.sync( options );
+
+		forEach( results, (result) => {
+			if ( result.hasChanged ) {
+				console.log( 'changed: ', result.file.replace( dirBase, '' ) );
+			}
+		});
+	}
+});

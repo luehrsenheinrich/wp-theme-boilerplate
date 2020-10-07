5e8f51bd4d5d2f8a555ae33718643e718652b739
Christopher Kanitz <ck@luehrsen-heinrich.de>
2020-10-07 11:46:06 +0200

    update setup syntax

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -24,7 +24,7 @@ done
 echo $(status_message "Connected to the localhost WordPress...")
 
 # Checking if WordPress is already installed
-if ! $(${WP_CLI} core is-installed); then
+if ! $(${WP_CLI} wp core is-installed); then
 
 	# Set the correct file permissions
 	docker-compose run --user=root --rm cli chown www-data -R /var/www/
@@ -33,25 +33,25 @@ if ! $(${WP_CLI} core is-installed); then
 
 	# Install WordPress
 	echo $(status_message "Installing WordPress...")
-	${WP_CLI} core install --url=localhost --title="WordPress Test" --admin_user=wordpress --admin_password=wordpress --admin_email=test@test.com
+	${WP_CLI} wp core install --url=localhost --title="WordPress Test" --admin_user=wordpress --admin_password=wordpress --admin_email=test@test.com
 
 	# Check for WordPress updates, just in case the WordPress image isn't up to date.
-	${WP_CLI} core update
+	${WP_CLI} wp core update
 
 	# Activate Agncy
-	${WP_CLI} theme activate _lhtbp
+	${WP_CLI} wp theme activate _lhtbp
 
 	# Import and activate needed plugins
-	${WP_CLI} plugin install gutenberg wordpress-importer query-monitor debug-bar theme-check wp-munich-blocks --activate
+	${WP_CLI} wp plugin install gutenberg wordpress-importer query-monitor debug-bar theme-check wp-munich-blocks --activate
 
 	echo $(status_message "Downloading WordPress theme unit test data...")
 	${WP_CLI} curl -O https://raw.githubusercontent.com/WPTRT/theme-unit-test/master/themeunittestdata.wordpress.xml >/dev/null 2>&1
 
 	echo $(status_message "Importing WordPress theme unit test data...\n")
-	${WP_CLI} import themeunittestdata.wordpress.xml --authors=create
+	${WP_CLI} wp import themeunittestdata.wordpress.xml --authors=create
 
 	# Activate debugging
-	${WP_CLI} config set WP_DEBUG true --raw
+	${WP_CLI} wp config set WP_DEBUG true --raw
 
 	docker-compose run --user=root wordpress chown www-data -R /var/www/
 
